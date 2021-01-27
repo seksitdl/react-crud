6df60e1933e2c9625b2816c27177bf05f7adc58f
seksitdl <seksit.to@gmail.com>
2021-01-27 22:28:56 +0700

    main

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
   return (
     <div className="App container">
       <h1>Employee Information</h1>
@@ -15,6 +52,9 @@ function App() {
               type="text"
               className="form-control"
               placeholder="Enter Name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -25,6 +65,9 @@ function App() {
               type="number"
               className="form-control"
               placeholder="Enter Age"
+              onChange={(event) => {
+                setAge(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -35,6 +78,9 @@ function App() {
               type="text"
               className="form-control"
               placeholder="Enter Country"
+              onChange={(event) => {
+                setCountry(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -45,6 +91,9 @@ function App() {
               type="text"
               className="form-control"
               placeholder="Enter Position"
+              onChange={(event) => {
+                setPosition(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -55,14 +104,33 @@ function App() {
               type="number"
               className="form-control"
               placeholder="Enter wage"
+              onChange={(event) => {
+                setWage(event.target.value);
+              }}
             ></input>
           </div>
-          <button className="btn btn-success">Add Employee</button>
+          <button className="btn btn-success" onClick={addEmployee}>Add Employee</button>
         </form>
       </div>
-      <hr/>
+      <hr />
       <div className="employees">
-        <button className="btn btn-primary">Show employees</button>
+        <button className="btn btn-primary" onClick={getEmployees}>
+          Show employees
+        </button>
+        <br></br>
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee card">
+              <div className="card-body text-left">
+                <p className="card-text">Name: {val.name}</p>
+                <p className="card-text">Age: {val.age}</p>
+                <p className="card-text">Country: {val.country}</p>
+                <p className="card-text">Position: {val.position}</p>
+                <p className="card-text">Wage: {val.wage}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
